7ca849fa0f23c5b39fbc2ddec0f03e19acbe7970
aschrae6 <adam.schraedel@ey.com>
2023-02-28 12:04:18 -0500

    package structure

--- src/main/java/com/monopolycapstone/UserServiceApplication.java
+++ src/main/java/com/monopolycapstone/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.monopolycapstone.userservice;
+package com.monopolycapstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
